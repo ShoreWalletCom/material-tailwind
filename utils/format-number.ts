256606eaa79987787a908de1657fee694e7c461c
Shore Wallet David <290729720+ShoreWalletCom@users.noreply.github.com>
2026-06-29 22:11:42 +0300

    Update format-number.ts

--- utils/format-number.ts
+++ utils/format-number.ts
@@ -1,22 +1,21 @@
-export function formatNumber(number, decPlaces) {
-  decPlaces = Math.pow(10, decPlaces);
+export function formatNumber(number, decPlaces = 0) {
+  if (typeof number !== "number" || isNaN(number)) return number;
 
+  const factor = Math.pow(10, decPlaces);
   const abbrev = ["K", "M", "B", "T"];
 
   for (let i = abbrev.length - 1; i >= 0; i--) {
-    var size = Math.pow(10, (i + 1) * 3);
+    const size = Math.pow(10, (i + 1) * 3);
 
-    if (size <= number) {
-      number = Math.round((number * decPlaces) / size) / decPlaces;
+    if (number >= size) {
+      let formatted = Math.round((number * factor) / size) / factor;
 
-      if (number == 1000 && i < abbrev.length - 1) {
-        number = 1;
+      if (formatted === 1000 && i < abbrev.length - 1) {
+        formatted = 1;
         i++;
       }
 
-      number += abbrev[i];
-
-      break;
+      return formatted + abbrev[i];
     }
   }
 
