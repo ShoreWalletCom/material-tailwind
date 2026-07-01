0be3838237f7c12035e4b9f98e188fbc8a8efa82
Shore Wallet David <290729720+ShoreWalletCom@users.noreply.github.com>
2026-07-01 12:07:37 +0300

    Update copy-to-clipboard.ts

--- utils/copy-to-clipboard.ts
+++ utils/copy-to-clipboard.ts
@@ -2,21 +2,19 @@ export function copyToClipboard(text) {
   return new Promise<void>((resolve, reject) => {
     if (navigator?.clipboard) {
       const cb = navigator.clipboard;
-
       cb.writeText(text).then(resolve).catch(reject);
     } else {
       try {
         const body = document.querySelector("body");
-
         const textarea = document.createElement("textarea");
-        body?.appendChild(textarea);
-
         textarea.value = text;
+        textarea.readOnly = true;
+        textarea.style.position = "fixed";
+        textarea.style.left = "-9999px";
+        body?.appendChild(textarea);
         textarea.select();
         document.execCommand("copy");
-
         body?.removeChild(textarea);
-
         resolve();
       } catch (e) {
         reject(e);
@@ -24,5 +22,4 @@ export function copyToClipboard(text) {
     }
   });
 }
-
 export default copyToClipboard;
